66a70a0c34b0322aa69bcf9a9c3876f9f118366b
MathisDupray <mathis.dupray@sts-sio-caen.info>
2023-12-22 20:03:13 +0100

    f

--- Docs/pages/php/contact.php
+++ Docs/pages/php/contact.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require_once __DIR__ . '../vendor/phpmailer/phpmailer/src/SMTP.php';
+require_once __DIR__ . './../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require_once __DIR__ . './../vendor/phpmailer/phpmailer/src/SMTP.php';
 use PHPMailer\PHPMailer;
 use \PHPMailer\PHPMailer\SMTP;
 
