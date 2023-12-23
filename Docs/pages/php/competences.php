3303bd9147a5e1c91cedec26002b57f50061a436
MathisDupray <mathis.dupray@sts-sio-caen.info>
2023-12-23 18:08:03 +0100

    test phpyaml

--- Docs/pages/php/competences.php
+++ Docs/pages/php/competences.php
@@ -6,35 +6,29 @@ echo "<div class='tables'>";
 foreach ($data as $domaine) {
     echo "<div class='domaine'><h2>".$domaine['domaine']."</h2>\n";
 
-    // Check if the 'competences' key is present and if it's an array
-    if (isset($domaine["competences"]) && is_array($domaine["competences"])) {
-        foreach ($domaine["competences"] as $competence => $niveau) {
-            // Convert competence name to lowercase and remove spaces
-            $competenceLowerCase = str_replace(' ', '', strtolower($competence));
+    foreach ($domaine["competences"] as $competence => $niveau) {
+        // Convert competence name to lowercase and remove spaces
+        $competenceLowerCase = str_replace(' ', '', strtolower($competence));
 
-            echo "<div class='competence'>";
-            echo "<p class='competence-name'>".$competence."</p>";
+        echo "<div class='competence'>";
+        echo "<p class='competence-name'>".$competence."</p>";
 
-            // Check if $niveau is an integer
-            if (is_int($niveau)) {
-                echo "<div class='progress-bar'>";
-                echo "<div class='progress'><div class='gradient-bar' style='width: ".($maxSkillLevel * $niveau)."%;'></div></div>";
-                echo "</div>";
+        // Check if $niveau is a number (integer or floating-point)
+        if (is_numeric($niveau)) {
+            echo "<div class='progress-bar'>";
+            echo "<div class='progress'><div class='gradient-bar' style='width: ".($maxSkillLevel * $niveau)."%;'></div></div>";
+            echo "</div>";
+        } else {
+            // If $niveau is a string, create a link for any non-numeric string
+            $pdfFilePath = "./../../assets/images/".$competenceLowerCase.".pdf";
+            if (file_exists($pdfFilePath)) {
+                echo "<div class='pdf-link'><a href='".$pdfFilePath."' target='_blank'>Lien vers certif</a></div>";
             } else {
-                // If $niveau is a string, check if the corresponding PDF file exists
-                $pdfFilePath = "./../../assets/images/".$competenceLowerCase.".pdf";
-                if (file_exists($pdfFilePath)) {
-                    echo "<div class='pdf-link'><a href='".$pdfFilePath."' target='_blank'>Lien vers certif</a></div>";
-                } else {
-                    echo "<div class='skill-description'>".$niveau."</div>";
-                }
+                echo "<div class='skill-description'>".$niveau."</div>";
             }
-
-            echo "</div>\n";
         }
-    } else {
-        // If 'competences' key is not present or not an array, handle it accordingly
-        echo "<p>No competences available for this domain.</p>";
+
+        echo "</div>\n";
     }
 
     echo "</div>";
