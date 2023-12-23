1f5f279b1b39a501c7beac46e4b2f72075a7f43b
MathisDupray <mathis.dupray@sts-sio-caen.info>
2023-12-23 01:50:38 +0100

    test

--- Docs/pages/php/contact.php
+++ Docs/pages/php/contact.php
@@ -6,8 +6,9 @@ require __DIR__ . './../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+use ReCaptcha\ReCaptcha;
 
-if (!empty($_POST['ok'])) {
+/*if (!empty($_POST['ok'])) {
     $mail = new PHPMailer(true);
     try {
         // Server settings
@@ -34,4 +35,44 @@ if (!empty($_POST['ok'])) {
     } catch (Exception $e) {
         echo 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
     }
+}
+*/
+$gRecaptchaResponse = $_POST['g-recaptcha-response'];
+$recaptcha = new \ReCaptcha\ReCaptcha("6LeK6DkpAAAAAFtqZa4mfLO9NEegw9uOEALANwqP");
+$resp = $recaptcha->setExpectedHostname('recaptcha-demo.appspot.com')
+                  ->setExpectedAction('homepage')
+                  ->setScoreThreshold(0.5)
+                  ->verify($gRecaptchaResponse, $remoteIp);
+
+if ($resp->isSuccess()) {
+    if (!empty($_POST['ok'])) {
+        $mail = new PHPMailer(true);
+        try {
+            // Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;   // Enable verbose debug output
+            $mail->isSMTP();                         // Send using SMTP
+            $mail->Host = 'smtp.gmail.com';          // Set the SMTP server to send through
+            $mail->SMTPAuth = true;                  // Enable SMTP authentication
+            $mail->Username = 'mathis.dupray@sts-sio-caen.info';  // SMTP username
+            $mail->Password = 'yourPassword';        // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable implicit TLS encryption
+            $mail->Port = 465;                       // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+            // Recipients
+            $mail->setFrom('mathis.dupray@sts-sio-caen.info', 'Mailer');
+            $mail->addAddress($_POST['to'] ?? 'mathis.dupray@sts-sio-caen.info'); // Add a recipient
+    
+            // Content
+            $mail->isHTML(true);  // Set email format to HTML
+            $mail->Subject = ($_POST['subject'] ?? '') . ($_POST['nom'] ?? 'Subject');
+            $mail->Body = $_POST['body'] ?? 'This is the HTML message body <b>in bold!</b>';
+    
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+        }
+    }
+} else {
+    $errors = $resp->getErrorCodes();
 }
