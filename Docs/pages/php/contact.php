12e004f8237cce96578605b54c0323880c01ef15
MathisDupray <mathis.dupray@sts-sio-caen.info>
2023-12-23 01:44:22 +0100

    test

--- Docs/pages/php/contact.php
+++ Docs/pages/php/contact.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once __DIR__ . './../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require_once __DIR__ . './../vendor/phpmailer/phpmailer/src/SMTP.php';
+/*require_once __DIR__ . './../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require_once __DIR__ . './../vendor/phpmailer/phpmailer/src/SMTP.php';*/
+require __DIR__ . './../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
