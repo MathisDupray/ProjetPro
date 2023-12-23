7095a4e28e4302851854839368bea1b77d9d11bf
MathisDupray <mathis.dupray@sts-sio-caen.info>
2023-12-23 18:35:18 +0100

    test

--- Docs/pages/php/competences.php
+++ Docs/pages/php/competences.php
@@ -12,22 +12,22 @@ foreach ($data as $domaine) {
         // Créer une variable pour les certifs, "SecNum Academie" --> "secnumacademie", pour chemin vers certif.pdf
         $competenceLowerCase = str_replace(' ', '', strtolower($competence));
         echo "<div class='competence'>";
-        
-        if (is_numeric($niveau)) {
-            echo "<p class='competence-name'>".$competence."</p>";
-            echo "<div class='progress-bar'>";
-            echo "<div class='progress'><div class='gradient-bar' style='width: ".($maxSkillLevel * $niveau)."%;'></div></div>";
-            echo "</div>";
-        } else {
-            echo "<p class='competence-name' style='text-align: center;'>".$competence."</p>";
+        echo "<p class='competence-name'>".$competence."</p>";
+
+        if (!is_numeric($niveau)) {
+            echo "<div class='skill-description' style='text-align: center;'>";
             // Si chaine de caracteres, alors verif' si un fichier de certif relatif à la competence existe
             $pdfFilePath = "./../../Docs/pages/php/" . $competenceLowerCase . ".pdf"; // Chemin vers le .pdf
-            echo "<div class='skill-description'>";
             if (file_exists($pdfFilePath)) {
                 echo "<a href='".$pdfFilePath."' target='_blank'>{$niveau}</a>";
             } else {
                 echo $niveau;
             }
+            echo "</div>";
+        } else {
+            // Numeric values
+            echo "<div class='progress-bar'>";
+            echo "<div class='progress'><div class='gradient-bar' style='width: ".($maxSkillLevel * $niveau)."%;'></div></div>";
             echo "</div>";
         }
 
