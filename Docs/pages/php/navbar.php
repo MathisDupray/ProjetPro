938de870eb249407665e0c465cf893bb31ff56ec
MathisDupray <mathis.dupray@sts-sio-caen.info>
2023-12-23 17:30:30 +0100

    media query navbar

--- Docs/pages/php/navbar.php
+++ Docs/pages/php/navbar.php
@@ -4,16 +4,16 @@ $style = "color: #ffffff;";
 echo "<div id='navbar'>";
 foreach ($data as $itemlist) {
     echo "<ul>";
-    for ($i = 1; $i <= 4; $i++) {
+    for ($i = 1; $i <= 4; $i++) { // $i <= 4 car 4 logos
         $item = "item" . $i;
         echo "<li><i class='" . $itemlist[$item] . "' style='" . $style . "'></i></li>";
     }
     echo "</ul>";
 
     echo "<ul>";
-    for ($i = 1; $i <= 6; $i++) {
+    for ($i = 1; $i <= 6; $i++) { // $i <= 6 car 6 rubriques
         $rubrique = "rubrique" . $i;
-        $href = strtolower(str_replace(' ', '', $itemlist[$rubrique])); // Remove spaces and convert to lowercase
+        $href = strtolower(str_replace(' ', '', $itemlist[$rubrique])); // Convertir le nom des rubriques en href de type "A propos" -> "apropos"
         echo "<li><a href='" . "#anchor".$href . "'>" . $itemlist[$rubrique] . "</a></li>";
     }
     echo "</ul>";
